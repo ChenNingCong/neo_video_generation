d9963c438b2325916eeadcaa4906e8c0e824328e
ChenNingCong <nchen3@wpi.edu>
2025-10-26 15:16:04 -0400

    Move mnist dataset to script

--- data_script/mnist.py
+++ data_script/mnist.py
@@ -1,20 +1,8 @@
-import os
-import os.path as osp
 import math
-import random
-import pickle
-import warnings
-
-import glob
-import h5py
 import numpy as np
-
-import torch
-import torch.utils.data as data
 import torch.nn.functional as F
-import torch.distributed as dist
-from torchvision.datasets.video_utils import VideoClips
-import pytorch_lightning as pl
+import torchvision
+from torch.utils.data import Dataset 
 
 def preprocess(video, resolution, sequence_length=None):
     # video: THWC, {0, ..., 255}
@@ -45,3 +33,38 @@ def preprocess(video, resolution, sequence_length=None):
     video -= 0.5
 
     return video
+
+class MovingMNISTGrayDataset(Dataset):
+    def __init__(self, data_dir, sequence_length, train=True, resolution=64):
+        super().__init__()
+        # moving mnist split the dataset by frames, not videos, this is counter-intuitive
+        
+        self.dataset = torchvision.datasets.MovingMNIST(root=data_dir, download=True)
+        self.train = train
+        self.sequence_length = sequence_length
+        self.resolution = resolution
+        l = int(0.9 * len(self.dataset))
+        if self.train:
+            self.offset = 0
+            self.l = l
+        else:
+            self.offset = l
+            self.l = len(self.dataset) - l
+
+    @property
+    def n_classes(self):
+        raise Exception('class conditioning not support for MovingMNISTDataset dataset')
+
+    def __len__(self):
+        return self.l
+
+    def __getitem__(self, idx):
+        assert idx < self.l
+        start = np.random.randint(low=0, high=20 - self.sequence_length)
+        # T, C, H, W
+        video = self.dataset[idx + self.offset][start:start + self.sequence_length].permute((0, 2,3,1))
+        assert video.shape[0] == self.sequence_length
+        # preprocess accepts only T, H, W, C inputs
+        # we repeat the input 3 times...it seems that this requires fewer modification
+        #video = video.repeat(1,1,1,3)
+        return dict(video=preprocess(video, self.resolution))
